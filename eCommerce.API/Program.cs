e5b47f8edd103a81f726445743c38813f46cfc89
Hitesh inti <hiteshinti@gmail.com>
2025-09-02 10:10:33 +0530

    swash bycle addes

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -4,6 +4,7 @@ using eCommerce.API.MiddleWare;
 using System.Text.Json.Serialization;
 using eCommerce.Core.Mappers;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,18 +17,23 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 builder.Services.AddAutoMapper(cfg => cfg.LicenseKey = "<License Key Here>", typeof(ApplicationUserMapping).Assembly);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
